19b10284e42985601990c716e67d684cfea9ecd1
Alejandro Gutierrez Fontan <60949620+truposky@users.noreply.github.com>
2025-06-25 22:21:16 +0200

    Clean up ASGI config

--- iot_server/routing.py
+++
@@ -1,8 +0,0 @@
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from django.urls import path
-from devices.consumers import DeviceConsumer
-
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-})
